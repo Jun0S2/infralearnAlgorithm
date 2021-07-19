347ca6ff7fb8609abcee046e2ff7e1e008f4ca15
User <User@DESKTOP-0JAJN03>
2021-07-20 01:15:36 +0900

    pick numbers

---
+++ InflearnAlgorithm/src/StringAlgorithm/getNum.java
@@ -0,0 +1,40 @@
+package StringAlgorithm;
+
+import java.util.Scanner;
+
+/**Date: 07/20/2021
+ * Author: June Park
+ * runs fine and returns correct result, however, 
+ * says its wrong..im lost
+ * */
+public class getNum {
+	/*Only read number from the input*/
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String input = sc.nextLine();
+	getNum n = new getNum();
+	//n.get(input);
+	System.out.print(n.removeZeroStart(n.get(input)));
+	
+}
+public String get(String input) {
+	//number values in ascii: ASCII value of digits [0 – 9] ranges from [48 – 57]
+	//change string to char array
+	char[] checkByAscii=input.toCharArray();
+	String result="";
+	for(char x : checkByAscii) {
+		result += (x>=48 && x<=57) ? x:"";
+		
+	}
+	
+	return result;
+}
+public String removeZeroStart(String a) {
+	String result = "";
+	if(a.charAt(0)=='0') {
+		result = a.substring(1);
+	}
+	else{result = a;}
+	return result;
+}
+}
